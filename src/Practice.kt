11523224adeafd223a4e86c41825ec763650fd8e
Minami Munakata <june28munazo@gmail.com>
2019-12-18 18:59:47 +0900

    Practice `when` statement.

--- src/Practice.kt
+++ src/Practice.kt
@@ -1,8 +1,14 @@
 fun main(args: Array<String>) {
-    print("\nYour fortune is: ${getFortuneCookie()}")
+//    print("\nYour fortune is: ${getFortuneCookie()}")
+    var fortune: String
+    for (i in 1..10) {
+        fortune = getFortune(getBirthday())
+        println("\nYour fortune is: $fortune")
+        if (fortune.contains("Take it easy")) break;
+    }
 }
 
-fun getFortuneCookie(): String {
+fun getFortune(birthday: Int): String {
     val fortunes = listOf(
         "You will have a great day!",
         "Things will go well for you today.",
@@ -12,9 +18,16 @@ fun getFortuneCookie(): String {
         "Take it easy and enjoy life!",
         "Treasure your friends because they are your greatest fortune."
     )
+
+    return when (birthday) {
+        28, 31 -> fortunes[0]
+        in 1..7 -> fortunes[5]
+        else -> fortunes[birthday.rem(fortunes.size)]
+    }
+}
+
+fun getBirthday(): Int {
     print("Enter your birthday: ")
-    val birthday = readLine()?.toIntOrNull() ?:1
-    return fortunes[birthday.rem(fortunes.size)]
-
-
+    val birthday = readLine()?.toIntOrNull() ?: 1
+    return birthday
 }
